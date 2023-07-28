888a4890bdca8253ed8a72b28a2369acbe8d2b25
kevin <kevin.fernandez@ntsprint.com>
2023-07-28 14:43:21 -0400

    firs commit

---
+++ src/Providers/KeycloakServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+
+class KeycloakServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //$this->publishes([__DIR__.'/../config/keycloak.php' => 'config/keycloak.php'], 'config');
+        //$this->mergeConfigFrom(__DIR__.'/../config/keycloak.php', 'keycloak');
+    }
+
+    public function register()
+    {
+        Auth::extend('keycloak', function ($app, $name, array $config) {
+            return new KeycloakG(Auth::createUserProvider($config['provider']), $app->request);
+        });
+    }
+}
